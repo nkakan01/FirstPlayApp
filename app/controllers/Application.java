d07633e9ee6f4b5fd4b6a93571df0af1c6ebb63c
Nagendra Kakani <nani.kakani@gmail.com>
2014-04-09 23:01:36 -0400

    First Commit Added New Files

---
+++ app/controllers/Application.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import models.*;
+import play.data.*;
+
+import views.html.*;
+
+public class Application extends Controller {
+	
+static Form<Task> taskForm = form(Task.class);
+  
+  public static Result index() {
+	  
+    return ok("FirstPlayApp");
+  }
+  
+  public static Result tasks() {
+	  return ok(
+	    views.html.index.render(Task.all(), taskForm)
+	  );
+	}
+
+  public static Result newTask() {
+	  Form<Task> filledForm = taskForm.bindFromRequest();
+	  if(filledForm.hasErrors()) {
+	    return badRequest(
+	      views.html.index.render(Task.all(), filledForm)
+	    );
+	  } else {
+	    Task.create(filledForm.get());
+	    return redirect(routes.Application.tasks());  
+	  }
+	}
+
+
+  public static Result deleteTask(Long id) {
+	  return TODO;
+  }
+
+}
